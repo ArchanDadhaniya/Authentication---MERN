c7abd0443e73d964046534b815c00ec6f318ab51
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-04 18:14:17 +0530

    routes for the singup, login, logout is done.

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+export const signup = async (req, res) => {
+    res.send('signup success');
+}
+
+export const login = async (req, res) => {
+    res.send('login success');
+}
+
+export const logout= async (req, res) => {
+    res.send('logout success');
+}
