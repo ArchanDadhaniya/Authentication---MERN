f1637bed7f5bb496a44fbc9e03f6d9d4472b836a
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-05 22:56:07 +0530

    successfully sending the verification mail

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -22,3 +22,27 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     }
 };
 
+
+
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "442593f1-0cb0-45ef-b13e-3073a7e76b7a",
+            template_variables: {
+                company_info_name: "Auth Company",
+                name: name,
+            },
+        });
+
+        console.log("Welcome email sent successfully", response);
+    } catch (error) {
+        console.error(`Error sending welcome email`, error);
+
+        throw new Error(`Error sending welcome email: ${error}`);
+    }
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import {login, signup, logout} from "../controllers/auth.controller.js";
+import {login, signup, logout,verifyEmail} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 
 
 router.post('/signup',signup);
+
+
+router.post("/verify-email",verifyEmail)
 
 router.post('/login', login);
 
