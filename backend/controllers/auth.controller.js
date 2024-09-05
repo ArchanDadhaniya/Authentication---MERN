36b9d364c2bc133d8735f792be31644230a729fd
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-05 23:31:37 +0530

    user can successfully log in.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -97,7 +97,36 @@ export const verifyEmail = async (req, res) => {
 
 
 export const login = async (req, res) => {
-  res.send("login success");
+  const { email, password } = req.body;
+  try{
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({ success: false, message: "Invalid Credentials" });
+    }
+    const isPasswordValid = await bcryptjs.compare(password,user.password);
+    if(!isPasswordValid){
+      return res.status(400).json({ success: false, message: "Invalid Credentials" });
+    }
+
+    generateTokenAndSetCookie(res,user._id);
+
+    user.lastlogin = new Date();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Login Successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      }
+    })
+
+  }catch(error){
+    console.log("error in login ", error);
+    res.status(400).json({ success: false, message: error.message });
+
+  }
 };
 
 export const logout = async (req, res) => {
