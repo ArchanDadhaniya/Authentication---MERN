97a8c9b034b1583965bed3c1117d7c521f8d789a
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-05 23:16:17 +0530

    user can successfully log out.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -101,5 +101,6 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.send("logout success");
+  res.clearCookie("token");
+  res.status(200).json({success: true, message: "User logged out" });
 };
